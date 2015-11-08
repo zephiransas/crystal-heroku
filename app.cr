5b3a7dfd403459dfe062034e0bd860ad977ffb65
zephiransas <zephiransas@gmail.com>
2015-11-08 13:20:56 +0900

    Get port from env

--- app.cr
+++ app.cr
@@ -1,8 +1,16 @@
 require "http/server"
+require "option_parser"
 
-server = HTTP::Server.new(8080) do |request|
+server_port = 8080
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |port|
+    server_port = port.to_i
+  end
+end
+
+server = HTTP::Server.new(server_port) do |request|
   HTTP::Response.ok "text/plain", "Hello world, got #{request.path}!"
 end
 
-puts "Listening on http://0.0.0.0:8080"
+puts "Listening on http://0.0.0.0:#{server_port}"
 server.listen
